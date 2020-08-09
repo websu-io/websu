183c9c43b9d96874aa9569659e71e41de27a65ab
Sam Stoelinga <sammiestoel@gmail.com>
2020-08-08 21:12:58 -0700

    Add test-docker.sh script

---
+++ test-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -xe
+
+docker build -t samos123/websu-api:latest .
+[ ! "$(docker ps -a | grep mongo)" ] && docker run --name mongo -d mongo:4
+docker run --cap-add SYS_ADMIN -p 8000:8000 -e MONGO_URI=mongodb://mongo:27017 --link mongo:mongo samos123/websu-api:latest
