2dff11cd10eb9fc836b4ce66851d016fdd99da55
Sam Stoelinga <sammiestoel@gmail.com>
2021-01-05 22:07:03 -0800

    Fix issue of missing data when using embedded struct

--- docs/docs.go
+++ docs/docs.go
@@ -106,7 +106,6 @@ var doc = `{
                     "type": "string"
                 },
                 "form_factor": {
-                    "description": "Optional parameter, possible values are desktop or mobile. If unset will default to desktop",
                     "type": "string",
                     "example": "desktop"
                 },
@@ -114,7 +113,6 @@ var doc = `{
                     "type": "string"
                 },
                 "location": {
-                    "description": "Optional parameter, default location will be used if not set",
                     "type": "string",
                     "example": "australia-southeast1"
                 },
@@ -126,12 +124,10 @@ var doc = `{
                     "type": "string"
                 },
                 "throughput_kbps": {
-                    "description": "Optional parameter, by default will be set to 1000 if omitted",
                     "type": "integer",
                     "example": 50000
                 },
                 "url": {
-                    "description": "Required parameter the URL of the website",
                     "type": "string",
                     "example": "https://www.google.com"
                 }
