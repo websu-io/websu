c798302e43ac8367850fc1a359d0e46fbcc85ab5
Sam Stoelinga <sammiestoel@gmail.com>
2021-01-23 17:45:22 -0800

    Add integration test for self-deploy option

--- test-docker.sh
+++ test-docker.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
-set -xe
+#!/usr/bin/env bash
 
 docker build -f build/Dockerfile_websu-api -t samos123/websu-api:latest .
 docker build -f build/Dockerfile_lighthouse-server-docker -t samos123/lighthouse-server-docker:latest .
-docker-compose stop
-docker-compose rm
-docker-compose up
+docker-compose up -d
