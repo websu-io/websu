dd65c549951b0b00d36b350031420260e99b09db
Sam Stoelinga <sammiestoel@gmail.com>
2021-01-23 20:59:47 -0800

    integration test fail on any failure

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+set -xe
+
+testURL() {
+  curl --silent --show-error --fail "$@"
+}
+
 if [ $1 = "unit" ]; then
   echo "Creating docker container on port 27018"
   docker run --name test-mongo -d -p 27018:27017 mongo
@@ -17,11 +23,10 @@ elif [ $1 = "integration" ]; then
   ./test-docker.sh
   echo "Sleeping 10 seconds to make sure all services are up"
   sleep 10
-  curl -f http://localhost:8000
-  curl -f http://localhost:8000/reports
-  curl -f -d '{"url": "https://www.google.com"}' localhost:8000/reports
+  testURL http://localhost:8000/
+  testURL http://localhost:8000/reports
+  testURL -d '{"url": "https://www.google.com"}' localhost:8000/reports
   echo "Integration tests passed"
-
 else
-    echo "Please run with './run-tests.sh unit' or './run-tests.sh integration'"
+  echo "Please run with './run-tests.sh unit' or './run-tests.sh integration'"
 fi
